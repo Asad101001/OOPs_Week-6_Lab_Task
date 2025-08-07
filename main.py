d3e53a1e52cc40a1b4a0c51cae91494742f9cdf9
Asad <muhammadasadk42@gmail.com>
2025-08-07 19:33:53 +0500

    Added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from App import App
+
+# MAINTAINING NEAT AND TIDY main.py FILE AND ALSO FOLLOWING SINGLE RESPONSIBILITY PRINCIPLE
+
+def __main__():
+    App().run()
+
+if __name__== "__main__":
+    __main__()
